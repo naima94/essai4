9020cf7f13e7e186e77330bfc2909adf2f9821aa
naima94 <naimatou.adambi@gmail.com>
2021-09-27 11:02:23 -0800

    Extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
+  mode: "jit",
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        snk: "#E1E8FF"
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
